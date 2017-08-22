3dcf8bd9aa905fefc8f4a7a2eae7b977bcbb0930
Senthilnathan Kirisanth <senthilnathan_k@3logixs.lk>
2017-08-22 10:16:08 +0530

    first commit

---
+++ src/firstapp/FirstApp.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstapp;
+
+/**
+ *
+ * @author senthilnathan_k
+ */
+public class FirstApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    static int val=45;
+    public static void main(String[] args) {
+        System.out.println(val);
+        String name = "application";
+        System.out.println(name);
+        // TODO code application logic here
+    }
+    
+}
